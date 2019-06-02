22cf53f2057e4c084e61fce8126d996697fb694d
thomkaar <thomkaar@gmail.com>
2019-06-02 00:09:05 -0700

    ebc and cbc work for bmp files

--- task1.py
+++ task1.py
@@ -3,49 +3,57 @@
 from Crypto.Cipher import AES
 from sys import argv
 from Crypto import Random
-
+from array import array
+from itertools import cycle
 
 def main(argv):
-   # ecb_encrypt(argv[1], argv[2])
+   #ecb_encrypt(argv[1], argv[2])
    cbc_encrypt(argv[1], argv[2])
  
 
 def ecb_encrypt(inFileName, outFileName):
    # open the file
-   file = open(inFileName, "r")
+   file = open(inFileName, "rb")
    outFile = open(outFileName, "wb")
    # Create the AES obj
    randomGen = Random.new()
-   obj = AES.new("0123456789123456")
-   
+   obj = AES.new(randomGen.read(16))
+
+
+   header, file = traverseBMPHeader(file)
+   outFile.write(header)
+    
    while(True):
       message = file.read(16)
-      if( message == ''):
-         print("We've broken")
-         break   
+      if( len(message) == 0):
+         break
       elif len(message) != 16:
-         print("We need to pad")
-         while(len(message) != 16):
-            message += "0"
-      obj = AES.new(randomGen.read(16))
+         notFullBlock = True
+         message = pkcs7(message)
       ciphertext = obj.encrypt(message)
       outFile.write(ciphertext)
    return
+
 
 
 def cbc_encrypt(inFileName, outFileName):
    
    key = Random.get_random_bytes(16) 
    aes = AES.new(key)
-   iv = Random.get_random_bytes(16)
+   iv = bytearray(Random.get_random_bytes(16))
 
-   infile = open(inFileName, 'r')
-   outfile = open(outFileName, 'w')
+   infile = open(inFileName, 'rb')
+   outfile = open(outFileName, 'wb')
+  
+   header, infile = traverseBMPHeader(infile)
+   outfile.write(header)
    
+
+ 
    lastMBlock = False
    
    # read the plaintext and pad if necessary to create block of 16 bytes
-   mblock0 = infile.read(16)
+   mblock0 = bytearray(infile.read(16))
    
    if len(mblock0) < 16:
       mblock0_padded = pkcs7(mblock0)
@@ -54,21 +62,21 @@ def cbc_encrypt(inFileName, outFileName):
       mblock0_padded = mblock0
    
    # xor message with IV and encrypt it
-   to_encrypt0 = iv ^ mblock0_padded
-   cipher0 = aes.encrypt(to_encrypt0)
+   to_encrypt0 = xor(mblock0_padded, iv)
+   cipher0 = aes.encrypt(bytes(to_encrypt0))
    outfile.write(cipher0)
    
    prev_cipher = cipher0
    while not lastMBlock:
       # read the plaintext and pad if necessary to create block of 16 bytes
-      mblock = infile.read(16)
-      if mblock == '':
+      mblock = bytearray(infile.read(16))
+      if len(mblock) == 0:
          break
       mblock_padded = pkcs7(mblock)
 
       # xor message with previous cipher and encrypt it
-      to_encrypt = prev_cipher ^ mblock_padded
-      new_cipher = aes.encrypt(to_encrypt)
+      to_encrypt = xor(mblock_padded, prev_cipher)
+      new_cipher = aes.encrypt(bytes(to_encrypt))
       
       # write the new cipher to the outfile
       outfile.write(new_cipher)
@@ -88,8 +96,18 @@ def pkcs7(plaintext):
    pad_utf8 = pad_char.decode('utf-8', 'raise')
    pad_str = ''
    for i in range(pad_len):
-      pad_str += pad_utf8
-   return plaintext + pad_str
+      plaintext += pad_char
+   return plaintext
+
+
+def traverseBMPHeader(file):
+   header = file.read(54)
+   return header, file
+
+
+def xor(x,y):
+    return bytearray([a^b for a, b in zip(x, cycle(y))])
+
 
 if __name__ == '__main__':
    main(argv)
